3f37799afb3276b73d201981bc7df776948bef50
Yaroslavus <yaroslav_sagan@mail.ru>
2020-04-09 00:31:20 +0300

    Add files via upload

---
+++ telescope_in_workshop_functions.py
@@ -0,0 +1,13 @@
+class mirror:
+    def __init__(self, number_kb, x_kb, y_kb, z_kb, alpha_YZ_kb, beta_XZ_kb, number_exp, x_dev_exp, y_dev_exp, f_exp):
+        """Constructor"""
+        self.number_kb = number_kb
+        self.x_kb = x_kb
+        self.y_kb = y_kb
+        self.z_kb = z_kb
+        self.alpha_YZ_kb = alpha_YZ_kb
+        self.beta_XZ_kb = beta_XZ_kb
+        self.number_exp = number_exp
+        self.x_dev_exp = x_dev_exp
+        self.y_dev_exp = y_dev_exp
+        self.f_exp = f_exp
